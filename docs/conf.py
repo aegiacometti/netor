97e0de7675ee18f0f1ab4e7c981da38b0e130da7
adrian <aegiacometti@hotmail.com>
2019-11-14 21:28:12 +0100

    docs readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -12,7 +12,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../netor/tinydb/scripts/'))
+sys.path.insert(0, os.path.abspath('../netor/tinydb/scripts'))
 
 
 # -- Project information -----------------------------------------------------
