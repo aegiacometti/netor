14e467ebdb221a3f51d44de2995e3180621e447c
adrian <aegiacometti@hotmail.com>
2019-12-11 17:53:13 +0700

    python slack

--- netor/ansible/playbooks/scripts/msg-slack.py
+++ netor/ansible/playbooks/scripts/msg-slack.py
@@ -1,12 +1,11 @@
 import sys
+import ast
 
 from slackclient import SlackClient
 
-#slack_token = os.environ["TQJ9F14QJ/BQRHDS8E5/pc2ij5CLgGZkjiJityhUuqc5"]
-#client = SlackClient(slack_token)
+slack_token_slack = "xoxb-834321038834-849046616935-UuWCCQcdoZ51sXlh3NrLCysL"
+client = SlackClient(slack_token_slack)
 
-text = str(sys.argv)
+text = str(sys.argv[1])
 
-print(text)
-
-#client.api_call('chat.postMessage', channel='test', text=text)
+client.api_call('chat.postMessage', channel='test', text=text)
